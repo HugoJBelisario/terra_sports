fcdb1832e87cebdb1bd5a7961bfe70eaaba0f272
hbelisario-dbacks <hugojbelisario@gmail.com>
2026-02-19 17:43:12 -0700

    Trunk angle.

--- app.py
+++ app.py
@@ -2932,7 +2932,7 @@ with tab_joint:
     joint_color_map.update({
         "Forward Trunk Tilt": "blue",
         "Lateral Trunk Tilt": "green",
-        "Trunk Angle": "#FFFFFF"
+        "Trunk Angle": "#E9FF70"
     })
     joint_color_map.update({
         "Pelvis Angle": "darkblue",
